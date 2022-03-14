a244018d7a3729432ae514d6baa5c085712e0214
Michael Tuntang <mike.tuntang547@gmail.com>
2022-03-14 01:06:37 -0700

    fixed files

---
+++ Classic Snake/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Classic_Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ResetSizeButton = new System.Windows.Forms.Button();
+            this.GameScreen = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.HighScoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GameScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.Lime;
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(570, 53);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(111, 52);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ResetSizeButton
+            // 
+            this.ResetSizeButton.BackColor = System.Drawing.Color.Aquamarine;
+            this.ResetSizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResetSizeButton.Location = new System.Drawing.Point(570, 111);
+            this.ResetSizeButton.Name = "ResetSizeButton";
+            this.ResetSizeButton.Size = new System.Drawing.Size(111, 52);
+            this.ResetSizeButton.TabIndex = 1;
+            this.ResetSizeButton.Text = "Reset";
+            this.ResetSizeButton.UseVisualStyleBackColor = false;
+            this.ResetSizeButton.Click += new System.EventHandler(this.ResetSizeButton_Click);
+            // 
+            // GameScreen
+            // 
+            this.GameScreen.BackColor = System.Drawing.Color.Silver;
+            this.GameScreen.Location = new System.Drawing.Point(28, 53);
+            this.GameScreen.Name = "GameScreen";
+            this.GameScreen.Size = new System.Drawing.Size(532, 630);
+            this.GameScreen.TabIndex = 3;
+            this.GameScreen.TabStop = false;
+            this.GameScreen.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdateGraphics);
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(567, 182);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(66, 20);
+            this.ScoreLabel.TabIndex = 4;
+            this.ScoreLabel.Text = "Score: ";
+            this.ScoreLabel.Click += new System.EventHandler(this.ScoreLabel_Click);
+            // 
+            // HighScoreLabel
+            // 
+            this.HighScoreLabel.AutoSize = true;
+            this.HighScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HighScoreLabel.Location = new System.Drawing.Point(566, 220);
+            this.HighScoreLabel.Name = "HighScoreLabel";
+            this.HighScoreLabel.Size = new System.Drawing.Size(103, 20);
+            this.HighScoreLabel.TabIndex = 5;
+            this.HighScoreLabel.Text = "High Score:";
+            this.HighScoreLabel.Click += new System.EventHandler(this.HighScoreLabel_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerTick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(748, 725);
+            this.Controls.Add(this.HighScoreLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.GameScreen);
+            this.Controls.Add(this.ResetSizeButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            ((System.ComponentModel.ISupportInitialize)(this.GameScreen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ResetSizeButton;
+        private System.Windows.Forms.PictureBox GameScreen;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label HighScoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Classic Snake/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classic_Snake
+{
+    public enum Directions
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    class Settings
+    {
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        public Directions CurrentDirection;
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            CurrentDirection = Directions.Right;
+        }
+    }
+}
